dcc63cf6eb95cba236fe7c9984146971089bda56
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-08-15 00:09:16 -0400

    Finish first ASST

--- getting-into-javascript/exercises/primer/ex.js
+++ getting-into-javascript/exercises/primer/ex.js
@@ -1,5 +1,10 @@
 // TODO: define addFavoriteBook(..) function
-
+function addFavoriteBook(bookName) {
+  let bookNameLower = bookName.toLowerCase();
+  if (!bookNameLower.includes("great")) {
+    favoriteBooks.push(bookName);
+  }
+}
 // TODO: define printFavoriteBooks() function
 
 var favoriteBooks = [];
@@ -11,3 +16,14 @@ addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
 // TODO: print out favorite books
+
+console.log(favoriteBooks.join(", "));
+
+function printFavoriteBooks() {
+  console.log(`Favorite Books: ${favoriteBooks.length}`);
+  for (let title of favoriteBooks) {
+    console.log(title);
+  }
+}
+
+printFavoriteBooks();
