65e72c993d3c8a4e9addeca960f91739da0f7cd8
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-08-16 23:55:30 -0400

    Finish 3 pillar excercise

--- getting-into-javascript/exercises/three-pillars/ex.js
+++ getting-into-javascript/exercises/three-pillars/ex.js
@@ -1,43 +1,50 @@
 class Bookshelf {
-	constructor() {
-		this.favoriteBooks = [];
-	}
+  constructor() {
+    this.favoriteBooks = [];
+  }
 
-	// TODO: define methods `addFavoriteBook(..)`
-	// and `printFavoriteBooks()`
+  // TODO: define methods `addFavoriteBook(..)`
+  // and `printFavoriteBooks()`
+  addFavoriteBook(bookName) {
+    if (!bookName.includes("Great")) {
+      this.favoriteBooks.push(bookName);
+    }
+  }
+
+  printFavoriteBooks() {
+    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+    for (let bookName of this.favoriteBooks) {
+      console.log(bookName);
+    }
+  }
 }
 
-function addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		favoriteBooks.push(bookName);
-	}
-}
-
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
-
-function loadBooks( /* .. */ ) {
-	// TODO: call fakeAjax( .. );
+function loadBooks(bookshelf) {
+  // TODO: call fakeAjax( .. );
+  if (bookshelf instanceof Bookshelf) {
+    fakeAjax(BOOK_API, (books) => {
+      books.forEach((book) => bookshelf.addFavoriteBook(book));
+      bookshelf.printFavoriteBooks();
+    });
+  }
 }
 
 var BOOK_API = "https://some.url/api";
 
-
 // ***********************
 
 // NOTE: don't modify this function at all
-function fakeAjax(url,cb) {
-	setTimeout(function fakeLoadingDelay(){
-		cb([
-			"A Song of Ice and Fire",
-			"The Great Gatsby",
-			"Crime & Punishment",
-			"Great Expectations",
-			"You Don't Know JS"
-		]);
-	},500);
+function fakeAjax(url, cb) {
+  setTimeout(function fakeLoadingDelay() {
+    cb([
+      "A Song of Ice and Fire",
+      "The Great Gatsby",
+      "Crime & Punishment",
+      "Great Expectations",
+      "You Don't Know JS",
+    ]);
+  }, 500);
 }
+
+let books = new Bookshelf();
+loadBooks(books);
